8431048493d84f7ff3510cd2b631f16fe39a968f
Fa-bula <fa_bula@mail.ru>
2015-03-31 23:44:36 +0300

    changes for heroku

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var ToDos = {};
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
